b6abe010532053e899b4b127444635d752e5c353
vdantaunang <vdantaunang@users.noreply.github.com>
2025-11-06 13:54:48 +0000

    Add main.tsx, service-provider-provider.js, template-responsive-src.go

---
+++ frontend/web/src/service-provider-provider.js
@@ -0,0 +1,5 @@
+export const config = { runtime: 'edge' };
+
+export const usesrc_mutations = () => { return {}; };
+
+
---
+++ frontend/web/src/template-responsive-src.go
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/secretmsg_fhe/frontend/web/src/template-responsive-src.go
+// Generated at: 2025-12-03T23:45:47.831Z
+
+
